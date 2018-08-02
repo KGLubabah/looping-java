8895cca0338fcb19a278ee837ed5c25ed5f6d469
USER PC <USER PC@DESKTOP-6ESNFKL>
2018-08-02 08:03:58 +0700

    For While Do While

---
+++ src/j08/Contoh01.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j08;
+
+public class Contoh01 {
+    public static void main(String[] args)
+    {
+       // PERULANGAN FOR
+        int x;
+        for(x = 1; x <= 5; x++);
+        {
+            System.out.println("Telkom School Malang");
+        }
+        
+        //PERULANGAN WHILE
+        int i = 1;
+        while(i <= 5);
+        {
+            System.out.println("Telkom School Malang");
+            i++;
+        }
+        
+        //PERULANGAN DO WHILE
+        int j =1;
+        do
+        {
+            System.out.println("Telkom School Malang");
+            j++;
+        }
+        while(j <= 5);
+    }
+}
